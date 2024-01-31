0b55d6994b3a175a6c0306c2f67a813c68514b84
Faizan <android1@zlatotech.com>
2024-02-01 00:55:55 +0530

    padding

--- src/SHA-256/Controller.ts
+++ src/SHA-256/Controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
+import { GetHash } from "./Helper";
 
 export class SHA256Controller{
     static async StoreHash(req:Request, res:Response){
+        const result = await GetHash(req.body.text)
         res.send(req.body)
     }
 }
