53acf63a3b12b72b44d2c8d2a7659ccb7b5df682
Faizan <android1@zlatotech.com>
2024-02-08 17:37:36 +0530

    hashing done

--- src/SHA-256/Controller.ts
+++ src/SHA-256/Controller.ts
@@ -1,12 +1,19 @@
 import { Request, Response } from "express";
 import { GetHash } from "./Helper";
 
-export class SHA256Controller{
-    static async StoreHash(req:Request, res:Response){
-        const result = await GetHash(req.body.text)
-        res.status(200).json({
-            text:req.body.text,
-            hash:result
-        })
+export class SHA256Controller {
+  static async StoreHash(req: Request, res: Response) {
+    try {
+      const result = await GetHash(req.body.text);
+      res.status(200).json({
+        text: req.body.text,
+        hash: result,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(200).json({
+        message: "some thing went wrong",
+      });
     }
+  }
 }
--- src/SHA-256/components/padWith0s.ts
+++ src/SHA-256/components/padWith0s.ts
@@ -3,12 +3,15 @@ export const paddingBits = async (text: string): Promise<string> => {
   var temp: string = text;
   temp = temp + "1";
 
-  const tempdata = 448 - (temp.length % 512);
-
+  var tempdata = 448 - (temp.length % 512);
+  // if templength is between 448 - 512
+  if (tempdata < 0) {
+    tempdata = 448 + (64-Math.abs(tempdata));
+  }
   var arrayOf0s = [...Array(tempdata).keys()].map((temp) => "0");
 
   var padded = temp + arrayOf0s.join("");
-
+  console.log(padded.length);
   // Append 64 bits to the end, where the 64 bits are a big-endian integer representing the length of the original input in binary.
   //basically a 64 bit string ending with binary of length of text
   var lengthToPad = 64 - (text.length >>> 0).toString(2).length;
